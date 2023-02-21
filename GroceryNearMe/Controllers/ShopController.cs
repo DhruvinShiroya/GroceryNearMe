32229297c91ca326d7a58d6f8960ba26352f06f3
Dhurvinkumar shiroya <shiroyadhruvinm@yahoo.com>
2023-02-21 00:36:40 -0500

    Added background color to header and footer and added link in navbar to shop by category you can browse by category and add category image and name to it.

---
+++ GroceryNearMe/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using GroceryNearMe.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryNearMe.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var result = _context.Categories.OrderBy(c => c.Name).ToList();
+
+                        
+
+            return View(result);
+        }
+    }
+}
